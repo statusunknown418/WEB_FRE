9c6ed718800492bca4a63f6250dbdea95823a629
Alvaro_Aquije <gonzalo_6824@outlook.com>
2021-03-23 20:37:20 -0500

    Added useful info here :D

---
+++ JS/DOCS/Modern.js
@@ -0,0 +1,7 @@
+const foo = "wow"
+
+//The better sweeter concatenation ${<any_variable>} isn't working :c dunno why 
+//JELP xd
+
+console.log("This is a test for what %c YOU can do");
+console.log('As well as some modern syntax such as ${foo}');
