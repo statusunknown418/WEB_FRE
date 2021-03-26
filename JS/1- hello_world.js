93f1d4e901c7135121fcc68ce4ef5ee40dc9b2b9
h4ck3r <samurai@Alvaros-iMac.local>
2021-03-26 14:20:59 -0500

    Just testing the auto-format on save

--- JS/1- hello_world.js
+++ JS/1- hello_world.js
@@ -7,5 +7,9 @@ var length = myStr.length
 firstletter = myStr[0]
 
 product = i * i * i * 546 * 654
+for (let i = 0; i < array.length; i++) {
+
+    console.log(i);
+}
 
 console.log(myStr + " " + length + " " + firstletter);
