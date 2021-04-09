abcddd229b3d05b376fc5f01bd49464788d6b2f5
Alvaro_Aquije <gonzalo_6824@outlook.com>
2021-04-09 10:00:54 -0500

    lol

---
+++ JS/12-Ternary.js
@@ -0,0 +1,8 @@
+//Instead of using a verbose if-statement we can implement the ternary operator
+
+var bar = 0
+
+function ternary(bar) {
+    var foo = 1 ? "it's 0" : "It's not"
+}
+console.log(ternary(bar));
